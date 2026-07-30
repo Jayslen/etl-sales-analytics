8e3702995ac86022d452fc91393d400d8c712ffa
Jayslen <jayslen134@gmail.com>
2026-07-29 20:43:19 -0400

    feat(ETL): Add csv conf properties to file config

--- ETL/src/conf.rs
+++ ETL/src/conf.rs
@@ -1,11 +1,24 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::fs;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 pub struct Config {
     pub api_url: String,
-    pub endpoints: Vec<String>,
-    pub csv: Vec<String>,
+    pub api: Api,
+    pub csv: Csv,
+}
+
+#[derive(Deserialize)]
+pub struct Api {
+    pub customers: String,
+    pub products: String,
+}
+
+#[derive(Deserialize)]
+pub struct Csv {
+    pub customers: String,
+    pub products: String,
+    pub orders: String,
 }
 
 pub fn load_config(file_path: &String) -> Result<Config, Box<dyn std::error::Error>> {
@@ -16,25 +29,58 @@ pub fn load_config(file_path: &String) -> Result<Config, Box<dyn std::error::Err
     Ok(config)
 }
 
-pub mod endpoints {
-    use serde::Deserialize;
+#[derive(Debug, Deserialize)]
+pub struct Products {
+    #[serde(rename = "ProductID", alias = "product_id")]
+    pub product_id: usize,
 
-    #[derive(Deserialize, Debug)]
-    pub struct Products {
-        product_id: usize,
-        product_name: String,
-        category_id: usize,
-        price: f64,
-        stock: usize,
-    }
+    #[serde(rename = "ProductName", alias = "product_name")]
+    pub product_name: String,
 
-    #[derive(Deserialize, Debug)]
-    pub struct Customers {
-        customer_id: usize,
-        first_name: String,
-        last_name: String,
-        email: String,
-        phone: String,
-        city_id: usize,
-    }
+    #[serde(rename = "CategoryID", alias = "category_id")]
+    pub category_id: usize,
+
+    #[serde(rename = "Price", alias = "price")]
+    pub price: f64,
+
+    #[serde(rename = "Stock", alias = "stock")]
+    pub stock: usize,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Customers {
+    #[serde(rename = "CustomerID", alias = "customer_id")]
+    pub customer_id: usize,
+
+    #[serde(rename = "FirstName", alias = "first_name")]
+    pub first_name: String,
+
+    #[serde(rename = "LastName", alias = "last_name")]
+    pub last_name: String,
+
+    #[serde(rename = "Email", alias = "email")]
+    pub email: String,
+
+    #[serde(rename = "Phone", alias = "phone")]
+    pub phone: String,
+
+    // API gives city_id, CSV gives city name
+    #[serde(alias = "city_id")]
+    pub city: Option<String>,
+
+    #[serde(default)]
+    pub country: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct Order {
+    #[serde(rename = "OrderID")] // 👈 handle ID edge case
+    pub order_id: usize,
+
+    #[serde(rename = "CustomerID")] // 👈 same here
+    pub customer_id: usize,
+
+    pub order_date: String,
+    pub status: String,
 }
